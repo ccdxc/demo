ceb50bb54e13a61f3d3ae8888608a96451c61300
cswxw <912175520@qq.com>
2018-06-07 16:43:45 +0800

    update python demo about [check dict to remove line that less then 8]

--- python/searchDir/1.py
+++ python/searchDir/1.py
@@ -1,4 +1,4 @@
-# check dict to remove line that less then 8
+
 
 import os
 
@@ -19,11 +19,11 @@ def mkdir(path):
 
         os.makedirs(path) 
  
-        print path+' success'
+        #print path+' success'
         return True
     else:
 
-        print path+' has exist'
+        #print path+' has exist'
         return False
 
 def writeNew(old,new,newdir):
@@ -37,10 +37,106 @@ def writeNew(old,new,newdir):
     f.close();
     f2.close();
         
+def filesort(old , new,newdir):
+    print old, ' '  , new
+    f=open(old,"r")
+    mkdir(newdir)
+    f2=open(new,"w")
 
-rootdir= "d:\dict3"
-newdir= "d:\dict3_new"
+    lists=f.readlines();
+    
+    lists = list(set(lists))
+    
+    lists.sort();
+    f2.writelines(lists);
+    
+    f.close();
+    f2.close();
 
-for root, dirs, files in os.walk(rootdir):
-    for file in files:
-        writeNew(os.path.join(root,file),os.path.join(root.replace(rootdir,newdir),file),root.replace(rootdir,newdir))
+rootdir= "d:\dict3_n"
+#newdir= "d:\dict3_new"
+
+
+def filter(a,b):
+    lists=list(set(b).difference(set(a).intersection(b)))
+    lists.sort();
+    return lists;
+
+def getAllfilepath(path):
+    lists=[]
+    for dirpath,dirnames,filenames in os.walk(path):
+        for file in filenames:
+            fullpath=os.path.join(dirpath,file)
+            lists.append(fullpath)
+    return lists;
+
+
+dic = getAllfilepath(rootdir)
+
+
+for i in range(0,len(dic)):
+	f=open(dic[i],"r")
+	a=f.readlines();
+	f.close()
+	print dic[i]
+	for j in range(i+1,len(dic)):
+	    #b
+		old2 = dic[j]
+		f2=open(old2,"r")
+		b=f2.readlines();
+		f2.close()
+		#new = os.path.join(root.replace(rootdir, newdir),files[j])
+		#tempdir = root.replace(rootdir,newdir);
+		#print old,' ',old2 , ' ' ,  new
+		#mkdir(tempdir)
+		print '\t',old2
+		f2=open(old2,"w")
+		f2.writelines(filter(a,b));
+		f2.close()
+
+
+'''
+dic = os.walk(rootdir);
+for root, dirs, files in dic:
+    for i in range(0,len(files)):
+        # writeNew(os.path.join(root,file),os.path.join(root.replace(rootdir,newdir),file),root.replace(rootdir,newdir))
+        #filesort(os.path.join(root,file),os.path.join(root.replace(rootdir,newdir),file),root.replace(rootdir,newdir))
+
+        # check
+        #if i+1 >= len(files):
+        #    break;
+        
+        #a
+        old = os.path.join(root,files[i]);
+        f=open(old,"r")
+        a=f.readlines();
+        print old
+        for j in range(i+1,len(files)):
+            #b
+            old2 = os.path.join(root,files[j]);    
+            f2=open(old2,"r")
+            b=f2.readlines();
+            f2.close()
+            
+            #new = os.path.join(root.replace(rootdir, newdir),files[j])
+            #tempdir = root.replace(rootdir,newdir);
+            #print old,' ',old2 , ' ' ,  new
+            #mkdir(tempdir)
+            print '\t',old2
+            
+            f2=open(old2,"w")
+            f2.writelines(filter(a,b));
+            f2.close()
+
+            
+            
+        f.close()
+        
+  '''          
+            
+                    
+
+
+
+
+        
